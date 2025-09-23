eb981f39d24e266802d940c63479fa64146a8a74
uncor3 <contactuncor3@gmail.com>
2025-09-23 02:52:58 +0000

    cleanup

--- rpiplay.cpp
+++ rpiplay.cpp
@@ -286,3 +286,32 @@ int stop_server()
     logger_destroy(render_logger);
     return 0;
 }
+
+extern "C" {
+int start_server_qt(const char *name)
+{
+    std::vector<char> hw_addr = DEFAULT_HW_ADDRESS;
+    video_renderer_config_t video_config;
+    video_config.background_mode = DEFAULT_BACKGROUND_MODE;
+    video_config.low_latency = DEFAULT_LOW_LATENCY;
+    video_config.rotation = DEFAULT_ROTATE;
+    video_config.flip = DEFAULT_FLIP;
+
+    audio_renderer_config_t audio_config;
+    audio_config.device = DEFAULT_AUDIO_DEVICE;
+    audio_config.low_latency = DEFAULT_LOW_LATENCY;
+
+    // Use Qt renderers
+    video_init_func = video_renderer_qt_init;
+    audio_init_func = audio_renderer_gstreamer_init;
+
+    return start_server(hw_addr, std::string(name), false, &video_config,
+                        &audio_config, 1920, 1080, 60.0);
+}
+
+int stop_server_qt()
+{
+    running = false;
+    return stop_server();
+}
+}
